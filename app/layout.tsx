19d44b92dc2aff1ae48d04e6cab814503e4089c3
GRIZLI-RGB <ek916569@gmail.com>
2026-05-26 02:44:42 +0500

    Добавил различные иконки

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,29 @@ import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "Ideapick",
+	description: "Ideapick",
+	metadataBase: new URL("https://ideapick.ru"),
+	openGraph: {
+		title: "Ideapick",
+		description: "Ideapick",
+		siteName: "Ideapick",
+		locale: "ru_RU",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Ideapick",
+		description: "Ideapick",
+	},
+	icons: {
+		other: [
+			{
+				rel: "mask-icon",
+				url: "/icons/safari-pinned-tab.svg",
+				color: "#000000",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
